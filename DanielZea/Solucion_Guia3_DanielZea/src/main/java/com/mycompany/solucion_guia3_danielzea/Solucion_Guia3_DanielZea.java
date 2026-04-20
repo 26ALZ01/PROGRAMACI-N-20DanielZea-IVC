b77a777c57ac2b1a7bf6bb6314d98f9e5e674052
26ALZ01-DanielZea <lopezalvaro16db@gmail.com>
2026-04-20 13:51:38 -0600

    Add files via upload

---
+++ DanielZea/Solucion_Guia3_DanielZea/src/main/java/com/mycompany/solucion_guia3_danielzea/Solucion_Guia3_DanielZea.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solucion_guia3_danielzea;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Solucion_Guia3_DanielZea {
+
+    public static void main(String[] args) {
+        jfrmMensajes Yoni=new jfrmMensajes();
+        Yoni.setVisible(true);
+    }
+}
